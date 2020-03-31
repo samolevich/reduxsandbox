3ecdc295c326dadefc9e7e7a57997836136e95ba
samolevich <samolevich@gmail.com>
2020-03-31 23:13:27 +0300

    trying createStore, dispatch and subscribe

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import {createStore} from 'redux';
+
 const reducer = (state = 0, action) => {
   switch (action.type) {
     case "INC":
@@ -7,9 +9,10 @@ const reducer = (state = 0, action) => {
   }
 };
 
-let state = reducer(undefined, {});
-console.log(state);
-state = reducer(state, { type: "INC" });
-console.log(state);
-state = reducer(state, { type: "INC" });
-console.log(state);
+const store = createStore(reducer);
+store.subscribe(() => {
+  console.log(store.getState());
+})
+
+store.dispatch({type: 'INC'});
+store.dispatch({type: 'INC'});
