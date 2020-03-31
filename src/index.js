d1afde81b1b23779f5b529af3ca67b469178f564
Konstantin Samolevich <42073618+samolevich@users.noreply.github.com>
2020-03-31 22:57:39 +0300

    first commit
    create-react-app
    npm install redux react-redux

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const reducer = (state = 0, action) => {
+  switch (action.type) {
+    case "INC":
+      return state + 1;
+    default:
+      return state;
+  }
+};
+
+let state = reducer(undefined, {});
+console.log(state);
+state = reducer(state, { type: "INC" });
+console.log(state);
+state = reducer(state, { type: "INC" });
+console.log(state);
